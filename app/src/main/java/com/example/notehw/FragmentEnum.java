4bf5e560c3b0985b6672f418f9bce0d7e563dcdc
Kortique <40215395+Kortique@users.noreply.github.com>
2021-07-24 23:24:36 +0300

    Реализация записной книжки

---
+++ app/src/main/java/com/example/notehw/FragmentEnum.java
@@ -0,0 +1,6 @@
+package com.example.notehw;
+
+public enum FragmentEnum {
+    LIST_OF_NOTES,
+    NOTE
+}
---
+++ app/src/main/java/com/example/notehw/OnItemSelectedListener.java
@@ -0,0 +1,6 @@
+package com.example.notehw;
+
+@FunctionalInterface
+public interface OnItemSelectedListener {
+    void onItemSelected(Note note);
+}
