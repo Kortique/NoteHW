d836a87f9645d311bdeb92597afa371309a15fc3
Kortique <40215395+Kortique@users.noreply.github.com>
2021-08-05 13:22:51 +0300

    Создана ветка HW10

--- app/src/main/java/com/example/notehw/ui/MainActivity.java
+++ app/src/main/java/com/example/notehw/ui/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         initView();
 
         CompositionRoot compositionRoot = ((CustomApplication) getApplication()).getCompositionRoot();
